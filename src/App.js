6793cad8d5c513543ed15acf2564cfe7d8f5ae85
ChaneaHamilton <chaneahamilton@gmail.com>
2025-05-12 15:11:40 -0600

    added search form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">My dictionary App</header>
-      <button className="btn btn-danger"> Hello </button>
+      <div className="container">
+        <header className="App-header">My dictionary App</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          <small>Coded by Chanea Hamilton</small>
+        </footer>
+      </div>
     </div>
   );
 }
