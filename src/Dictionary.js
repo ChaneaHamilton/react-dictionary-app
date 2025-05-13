aaf0d11d823198e372404100e8bd86fba1df8b05
ChaneaHamilton <chaneahamilton@gmail.com>
2025-05-13 11:13:24 -0600

    rendered the word and meaning

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data);
+    setResults(response.data);
   }
   function search(event) {
     event.preventDefault();
-
+    //https://www.shecodes.io/learn/apis/dictionary
     let apiKey = "aatbf0c2od53d41737ca6d7d30e64b01";
 
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
@@ -26,6 +28,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
